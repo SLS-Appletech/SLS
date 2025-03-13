fca3c545bdfe9f3f20b433e207ab12565c1116ad
SLS-Appletech <sayali@appletechconsultants.com>
2025-03-13 21:18:59 +0530

    Update index.php

--- app/views/client/properties/index.php
+++ app/views/client/properties/index.php
@@ -1,1 +1,32 @@
+<?php
+require_once '../../controllers/ClientPropertyController.php';
+$controller = new ClientPropertyController();
+$properties = $controller->listProperties();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Available Properties</title>
+</head>
+<body>
+    <h2>Property Listings</h2>
+    <table border="1">
+        <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Location</th>
+            <th>Details</th>
+        </tr>
+        <?php foreach ($properties as $property): ?>
+        <tr>
+            <td><?= $property['title'] ?></td>
+            <td><?= $property['price'] ?></td>
+            <td><?= $property['location'] ?></td>
+            <td><a href="view.php?id=<?= $property['id'] ?>">View</a></td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
 
