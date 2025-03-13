61c7b5aa23655fcfa0c2ebb1a07b36a72e89b877
SLS-Appletech <sayali@appletechconsultants.com>
2025-03-13 21:31:17 +0530

    Update authMiddleware.php

--- app/middleware/authMiddleware.php
+++ app/middleware/authMiddleware.php
@@ -1,1 +1,12 @@
+<?php
+session_start();
 
+class AuthMiddleware {
+    public static function checkAuthenticated() {
+        if (!isset($_SESSION['user'])) {
+            header("Location: /views/login.php");
+            exit();
+        }
+    }
+}
+?>
