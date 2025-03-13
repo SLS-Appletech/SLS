64dfe5aa79a49c4f73b4e4bf8faf499a0eab7766
SLS-Appletech <sayali@appletechconsultants.com>
2025-03-13 21:13:44 +0530

    add.php

--- app/views/agent/properties/add.php
+++ app/views/agent/properties/add.php
@@ -1,1 +1,26 @@
+<?php
+require_once '../../controllers/AgentPropertyController.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $controller = new AgentPropertyController();
+    $controller->addProperty();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Add Property</title>
+</head>
+<body>
+    <h2>Add New Property</h2>
+    <form method="POST">
+        <label>Title:</label> <input type="text" name="title" required><br>
+        <label>Description:</label> <textarea name="description"></textarea><br>
+        <label>Price:</label> <input type="number" name="price" required><br>
+        <label>Location:</label> <input type="text" name="location" required><br>
+        <button type="submit">Add Property</button>
+    </form>
+</body>
+</html>
 
